8f5c3bc65cca54040cb0dff8db2f222f3f4724f6
pragnasrivalishetti <pragnasrivalishetti@gmail.com>
2026-07-09 19:33:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const quiz = [
+
+{
+    question: "1. Which HTML tag is used to create a hyperlink?",
+    options: ["<a>", "<link>", "<href>", "<url>"],
+    answer: "<a>"
+},
+
+{
+    question: "2. Which CSS property is used to make text bold?",
+    options: ["font-weight", "text-style", "font-style", "text-weight"],
+    answer: "font-weight"
+},
+
+{
+    question: "3. Which JavaScript keyword is used to declare a variable?",
+    options: ["int", "var", "let", "Both var and let"],
+    answer: "Both var and let"
+},
+
+{
+    question: "4. Which method is used to select an element by its ID in JavaScript?",
+    options: [
+        "document.querySelector()",
+        "document.getElementById()",
+        "document.getElementsByClassName()",
+        "document.getElement()"
+    ],
+    answer: "document.getElementById()"
+},
+
+{
+    question: "5. Which CSS property is used to change the background color of an element?",
+    options: [
+        "color",
+        "background-color",
+        "bgcolor",
+        "background-image"
+    ],
+    answer: "background-color"
+}
+
+];
